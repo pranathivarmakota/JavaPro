3a3c9b9a7273cdb876a94761e0329eb475e1a1f4
pranathivarmakota <113531544+pranathivarmakota@users.noreply.github.com>
2024-08-12 16:05:49 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main{
   public static void main(String [] args){
     System.out.println(" ==Welcome to java project===");
+    Customer c1 = new Customer(101,"Moha","Bangalore","moha@gmail.com");
+     System.out.println(c1);
 }
 }
